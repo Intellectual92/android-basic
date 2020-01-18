0fb443d2bff33ee93de90c89909adbd13c20d9e0
JISUNGIN <jsi.develop@gmail.com>
2020-01-18 19:48:59 +0900

    Add: Chapter1
    첫째 마당. Hello! 안드로이드

---
+++ app/src/main/java/com/example/android_basic/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.android_basic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v){
+        Toast.makeText(this, "Toast 버튼이 눌렸어요", Toast.LENGTH_LONG).show();
+    }
+
+    public void onButton2Clicked(View v){
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(myIntent);
+    }
+
+    public void onButton3Clicked(View v){
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1000-1000"));
+        startActivity(myIntent);
+    }
+}
